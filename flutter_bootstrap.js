c2a623784bf71d66e2c2cd0f7dd1af08a82976e2
chj927il-arch <chj927il-arch@users.noreply.github.com>
2026-08-01 12:05:07 +0000

    deploy: cf5cce1d663efec2a55dccc5358b23684fbdff30

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3320285805" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1214733107" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
